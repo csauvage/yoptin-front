f7cbc2520d73a1815dfd6f76657931b99a3a4a4a
Clément SAUVAGE <hello@clementsauvage.me>
2019-08-04 07:45:33 -0700

    New Style

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import logo from '../../assets/logo.svg'
 import sendgrid from '../../assets/sendgrid.svg'
+import screen from '../../assets/Screen.png'
 import './App.sass'
 
 const App = () => {
   return (
-    <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <span className={'App_Logo__slogan'}>Supercharge your marketing email.</span>
-        <div className={'App__builtfor'}>
-          Built for
-          <img src={sendgrid} className="App_Logo--Sendgrid" alt="logo" />
+    <div className="Home">
+      <section className={"Header"}>
+
+        <h1>Managin' your marketing list with ease</h1>
+        <div className={'Dotted'}/>
+
+        <p>
+          Yopt'in is yet the easiest solution to manage your Sendgrid's marketing list from email collection to unsubscribes (it happen's...), the GDPR way
+        </p>
+        <div className={'Header__CTA'}>
+          August 2019
         </div>
 
-      <div className={'App__Availability'}>
-        August 2019.
-      </div>
+        <div className={'Header__For'}>
+          <span>Built for</span>
+          <img src={sendgrid}/>
+        </div>
+
+      </section>
+      <div className={"Header__Border"}/>
+      <section className={"Sample"}>
+
+        <img src={screen}/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet architecto cupiditate, debitis enim ex inventore minima molestiae natus nobis non odit placeat possimus quasi recusandae, saepe, totam veritatis vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A amet architecto cupiditate, debitis enim ex inventore minima molestiae natus nobis non odit placeat possimus quasi recusandae, saepe, totam veritatis vero.</p>
+
+      </section>
+
+
+      <div className={"Features__before"}/>
+      <section className={"Features"}>
+
+        <div className={'Feature'}>
+          <h2>Easy to use</h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur eaque eveniet ex iste itaque
+        </div>
+
+        <div className={'Feature'}>
+          <h2>RGPD Compilant</h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur eaque eveniet ex iste itaque
+        </div>
+
+        <div className={'Feature'}>
+          <h2>Easy to use</h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur eaque eveniet ex iste itaque
+        </div>
+
+      </section>
+      <div className={"Features__after"}/>
+
+
     </div>
   );
-}
+};
 
 export default App
